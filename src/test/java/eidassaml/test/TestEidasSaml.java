d750ea58d73f2090e31e380fd5ba8b50f95becf3
sander <s.ijpma@dictu.nl>
2020-03-24 08:29:56 +0100

    fix in UTF-8 encoding

--- src/test/java/eidassaml/test/TestEidasSaml.java
+++ src/test/java/eidassaml/test/TestEidasSaml.java
@@ -231,11 +231,11 @@ public class TestEidasSaml {
 		BirthNameAttribute birthName = new BirthNameAttribute("Meyer");
 		CurrentAddressAttribute currentAddress = new CurrentAddressAttribute("Am Fallturm","33","Bremen","28207", "100", "bla", "bla", "bla", "bla");
 		DateOfBirthAttribute dao = new DateOfBirthAttribute("1982-02-11");
-		FamilyNameAttribute familyName =  new FamilyNameAttribute("Muller", "Müller");
+		FamilyNameAttribute familyName =  new FamilyNameAttribute("Muller", "MÃ¼ller");
 		GenderAttribute gender = new GenderAttribute(GenderType.Male);
-		GivenNameAttribute givenName = new GivenNameAttribute("Bjorn", "Bjørn");
+		GivenNameAttribute givenName = new GivenNameAttribute("Bjorn", "BjÃ¸rn");
 		PersonIdentifierAttribute pi = new PersonIdentifierAttribute("test12321");
-		PlaceOfBirthAttribute pob = new PlaceOfBirthAttribute("Saint-Étienne, France");		
+		PlaceOfBirthAttribute pob = new PlaceOfBirthAttribute("Saint-Ã‰tienne, France");		
 		ArrayList<EidasAttribute> _att = new ArrayList<EidasAttribute>();
 		_att.add(birthName);
 		_att.add(currentAddress);
